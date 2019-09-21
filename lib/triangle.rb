035d5145452aaffbaede891b307a83fb160dd4bc
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-21 19:09:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_reader :side_a, :side_b, :side_c
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+  
+  def kind
+    
+  end
+  
+  class TriangleError < StandardError
+
+      
+    end
+  end
+  
+  
 end
